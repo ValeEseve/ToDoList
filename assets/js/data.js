9a0f1cfedb4fa902cff5f55ce86391a5fbcde48c
Valentina Segura Vargas <vale.eseve@gmail.com>
2025-04-25 00:29:52 -0400

    Base creada

---
+++ assets/js/data.js
@@ -0,0 +1,6 @@
+let dataTareas = [
+    {id: 1, tarea: "Realizar desafío", realizada: true}
+    {id: 2, tarea: "Devolver libro a biblioteca", realizada: false}
+    {id: 3, tarea: "Leer guía", realizada: false}
+    {id: 4, tarea: "Responder mail a Martina R.", realizada: true}
+]
